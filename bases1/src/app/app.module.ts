cad5e0528a9267e9220a9c3ef202b50a793f736c
edwinaguilarariza <edwinaguilarariza1@yahoo.com>
2021-07-10 17:01:35 -0500

    segundo commit

--- bases1/src/app/app.module.ts
+++ bases1/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 
@@ -19,7 +20,8 @@ import { ContadorModule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
      
   ],
   providers: [],
